29e6a2a0f0435a299e6dfab0d975e9b9fce9523e
micCi0 <120597140+micCi0@users.noreply.github.com>
2023-09-03 13:33:04 +0200

    Add files via upload

---
+++ breakout/breakout/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace breakout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.area = new System.Windows.Forms.Panel();
+            this.info = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.intervalBall = new System.Windows.Forms.Timer(this.components);
+            this.area.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // area
+            // 
+            this.area.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.area.Controls.Add(this.info);
+            this.area.Controls.Add(this.button1);
+            this.area.Location = new System.Drawing.Point(12, 12);
+            this.area.Name = "area";
+            this.area.Size = new System.Drawing.Size(572, 544);
+            this.area.TabIndex = 0;
+            this.area.Paint += new System.Windows.Forms.PaintEventHandler(this.playArea);
+            // 
+            // info
+            // 
+            this.info.AutoSize = true;
+            this.info.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.info.ForeColor = System.Drawing.Color.White;
+            this.info.Location = new System.Drawing.Point(201, 283);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(166, 36);
+            this.info.TabIndex = 1;
+            this.info.Text = "Game Over";
+            this.info.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(207, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.disabled);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 32;
+            this.timer.Tick += new System.EventHandler(this.startGame);
+            // 
+            // intervalBall
+            // 
+            this.intervalBall.Interval = 15;
+            this.intervalBall.Tick += new System.EventHandler(this.ballAnimate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(596, 577);
+            this.Controls.Add(this.area);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Breakout game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.movePlayer);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.stopPlayer);
+            this.area.ResumeLayout(false);
+            this.area.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel area;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer intervalBall;
+        private System.Windows.Forms.Label info;
+    }
+}
+
---
+++ breakout/breakout/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace breakout
+{
+    public partial class Form1 : Form
+    {
+        //blocks
+        int rows = 5;
+        int columns = 10;
+        int shift = 20;
+        int w = 60;
+        int h = 14;
+
+    // player
+        int x = 0;
+        int wPlayer = 80;
+        int y;
+        int hPlayer = 12;
+        int speed = 5;
+    //keys
+        bool left = false;
+        bool right = false;
+        bool game = false;
+
+    // ball
+        int ballX = 0;
+        int ballY = 0;
+        int ballSize = 20;
+        int ballSpeed = 1;
+        private int ballSpeedX = 1;
+        private int ballSpeedY = 1;
+        bool[,] blocks;
+
+
+        bool isOut = false;
+        bool over = false;
+        bool newGame = false;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBlocks();
+
+        }
+
+        private void InitializeBlocks()
+        {
+            // draw blocks
+            blocks = new bool[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    blocks[i, j] = true; // Initialize all blocks as present
+                }
+            }
+        }
+
+        private void playArea(object sender, PaintEventArgs e)
+        {
+
+            Color[] blockColors = { Color.Yellow, Color.Red, Color.White, Color.Green, Color.Orange };
+
+            Graphics g = e.Graphics;
+            
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (blocks[i, j])
+                    {
+                        //block
+
+                        Brush blockBrush = new SolidBrush(blockColors[i % blockColors.Length]);
+                        g.FillRectangle(blockBrush, j * (w + shift), i * (h + shift), w, h);
+                        blockBrush.Dispose();
+                    }
+                }
+            }
+
+            
+            // player
+            Brush playerColor = new SolidBrush(Color.White);
+            y = area.Height - hPlayer;
+            g.FillRectangle(playerColor, x, y, wPlayer, hPlayer);
+
+            // ball
+            Brush ballColor = new SolidBrush(Color.DarkGray);
+            if (!game) return;
+            g.FillEllipse(ballColor, ballX, ballY, ballSize, ballSize);
+
+            if (game)
+            {
+                intervalBall.Start();
+            }
+        }
+
+        private void movePlayer(object sender, KeyEventArgs e)
+        {
+            // keydown event
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    left = true;
+                    break;
+
+                case Keys.D:
+                    right = true;
+                    break;
+            }
+        }
+
+        private void stopPlayer(object sender, KeyEventArgs e)
+        {
+           // keyup event
+
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    left = false;
+                    timer.Start();
+                    break;
+
+                case Keys.D:
+                    right = false;
+                    timer.Start();
+                    break;
+            }
+        }
+
+        private void startGame(object sender, EventArgs e)
+        {
+            if (game)
+            {
+                timer.Enabled = true;
+                if (left && !right && x > 0)
+                {
+                    x -= speed;
+                }
+                if (right && !left && x < area.Width - wPlayer)
+                {
+                    x += speed;
+                }
+                Refresh();
+            }
+        }
+
+        private void disabled(object sender, EventArgs e)
+        {
+            // start game button click
+            if (!newGame)
+            {
+                button1.Visible = false;
+                game = true;
+            }
+            else
+            {
+                button1.Text = "Play again";
+
+                restart();
+                
+                
+                
+            }
+        }
+
+        private void restart()
+        {
+            // Reset game-related variables
+            game =true;
+            left = false;
+            right = false;
+            isOut = false;
+            over = false;
+
+            // remove label and button
+
+            info.Visible = false;
+            button1.Visible = false;
+
+            // Reset player and ball positions
+            x = 0;
+            y = area.Height - hPlayer;
+            ballX = 0;
+            ballY = 0;
+
+            // Reset block status
+            InitializeBlocks();
+
+            // Restart the game timers
+            intervalBall.Start();
+            timer.Start();
+
+            // Refresh the form to update the display
+            Refresh();
+        }
+
+
+        private void ballAnimate(object sender, EventArgs e)
+        {
+            ballX += ballSpeedX;
+            ballY += ballSpeedY;
+
+            if (ballX >= this.Width - ballSize || ballX < 0)
+            {
+                ballSpeedX *= -1; // Reverse horizontal direction
+                ballSpeedY = 1;   // Bounce vertically
+
+            }
+
+            if (ballY < 0)
+            {
+                ballSpeedY *= -1;
+                ballSpeedX = 2;
+            }
+             if (ballY+ballSize/2 >= area.Height + ballSize)
+            {
+               // out of 
+                over = true;
+              
+                intervalBall.Stop();
+                timer.Stop();
+            }
+            else if (ballY + ballSize >= y && ballX > x && ballX < x + wPlayer)
+            {
+                int hitPosition = ballX - x + ballSize / 2;
+                float normalizedHitPosition = (float)hitPosition / wPlayer * 2 - 1;
+                ballSpeedX = (int)(normalizedHitPosition * ballSpeed);
+                ballSpeedY *= -1;
+                ballY = y - ballSize;
+                isOut = true; 
+            }
+            // Check if the ball is out of bounds below the player
+            if (ballY > area.Height + ballSize)
+            {
+                intervalBall.Stop();
+                timer.Stop();
+                info.Visible = true;
+                newGame = true;
+                button1.Visible = true;
+            }
+            CheckBlockCollisions();
+
+            Refresh();
+        }
+
+        private void CheckBlockCollisions()
+        {
+            // collision between ball and blocks
+            if (!isOut) return;
+            int blockWidth = w;
+            int blockHeight = h;
+            int ballCenterX = ballX + ballSize / 2;
+            int ballCenterY = ballY + ballSize / 2;
+            int rowHit = -1;
+            int colHit = -1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (blocks[i, j])
+                    {
+                        if (over) return;
+                        int blockX = j * (blockWidth + shift);
+                        int blockY = i * (blockHeight + shift);
+
+                        if (ballCenterX >= blockX && ballCenterX <= blockX + blockWidth
+                            && ballCenterY >= blockY && ballCenterY <= blockY + blockHeight)
+                        {
+                            rowHit = i;
+                            colHit = j;
+                            blocks[i, j] = false;
+
+                            ballSpeedY *= -1;
+                            ballSpeedX = 2;
+                        }
+                    }
+                }
+            }
+
+           
+       
+    }
+}
+}
